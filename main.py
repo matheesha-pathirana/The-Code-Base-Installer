c20efd3dda6a97f786aeca2efc387cc08a445462
matheesha <pathiranamatheesha@gmail.com>
2022-04-15 18:12:05 +0530

    update to 0.0.3v

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import wget
-url = "https://www.python.org/static/img/python-logo@2x.png"
-wget.download(url, 'c:/users/LikeGeeks/downloads/pythonLogo.png')
+url = "https://raw.githubusercontent.com/matheesha-pathirana/The-Code-Base-Installer/master/version?token=GHSAT0AAAAAABRGIOISZLPRA4MJQI7HG55AYSZM6VA"
+wget.download(url, 'version.txt')
+f=open('version.txt')
+read=f.read()
+if read=='0.0.2':
+    print('You are uptodate!')
+
+else:
+    print('Please update to the latest version!')
